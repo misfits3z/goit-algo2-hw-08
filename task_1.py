494655f3ec91a9e480089e5c302b790907c543a5
Didenko Victoria <155799784+misfits3z@users.noreply.github.com>
2025-05-18 22:21:17 +0800

    task_1 done

--- task_1.py
+++ task_1.py
@@ -4,20 +4,57 @@ import time
 from collections import deque
 
 class SlidingWindowRateLimiter:
+
     def __init__(self, window_size: int = 10, max_requests: int = 1):
-        pass
+        self.window_size = window_size
+        self.max_requests = max_requests
+        self.user_requests: Dict[str, deque] = {}
 
     def _cleanup_window(self, user_id: str, current_time: float) -> None:
-        pass
+        if user_id not in self.user_requests:
+            return
+
+        window = self.user_requests[user_id]
+        # Видаляємо всі повідомлення старші за межу вікна
+        while window and current_time - window[0] > self.window_size:
+            window.popleft()
+
+        # Якщо deque порожня видаляємо користувача
+        if not window:
+            del self.user_requests[user_id]
 
     def can_send_message(self, user_id: str) -> bool:
-        pass
+        current_time = time.time()
+        self._cleanup_window(user_id, current_time)
+
+        if user_id not in self.user_requests:
+            return True
+
+        return len(self.user_requests[user_id]) < self.max_requests
 
     def record_message(self, user_id: str) -> bool:
-        pass
+        if self.can_send_message(user_id):
+            if user_id not in self.user_requests:
+                self.user_requests[user_id] = deque()
+            self.user_requests[user_id].append(time.time())
+            return True
+        return False
 
     def time_until_next_allowed(self, user_id: str) -> float:
-        pass
+        current_time = time.time()
+        self._cleanup_window(user_id, current_time)
+
+        if user_id not in self.user_requests:
+            return 0.0
+
+        window = self.user_requests[user_id]
+        if len(window) < self.max_requests:
+            return 0.0
+
+        oldest_timestamp = window[0]
+        time_left = self.window_size - (current_time - oldest_timestamp)
+        return max(0.0, time_left)
+
 
 # Демонстрація роботи
 def test_rate_limiter():
